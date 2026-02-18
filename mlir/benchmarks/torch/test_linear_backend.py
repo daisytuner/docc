b7a520c7a79800a197a7501d06cf23a7eb3fcf74
Atrisan <adrianschmitz2@gmail.com>
2026-02-18 15:47:50 +0100

    add interim test

---
+++ mlir/benchmarks/torch/test_linear_backend.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+from torch_mlir import fx
+
+
+class LinearNet(nn.Module):
+    def __init__(self, in_features: int, hidden_features: int, out_features: int):
+        super().__init__()
+        self.linear1 = nn.Linear(in_features, hidden_features, bias=False)
+        self.linear2 = nn.Linear(hidden_features, out_features, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        h1 = self.linear1(x)
+        h2 = self.linear2(h1)
+        return h2
+
+
+model = LinearNet(in_features=10, hidden_features=16, out_features=3)
+example_input = torch.randn(8, 10)
+
+torch_mlir = fx.export_and_import(
+    model, example_input, output_type=fx.OutputType.LINALG_ON_TENSORS
+)
+print(str(torch_mlir))
