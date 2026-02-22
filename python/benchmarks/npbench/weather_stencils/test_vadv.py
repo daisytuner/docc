c33c065a533303a117b15272352d611c8defd24c
Lukas Truemper <lukas.truemper@outlook.de>
2026-02-22 18:56:50 +0100

    updates verifier

--- python/benchmarks/npbench/weather_stencils/test_vadv.py
+++ python/benchmarks/npbench/weather_stencils/test_vadv.py
@@ -131,15 +131,15 @@ def kernel(utens_stage, u_stage, wcon, u_pos, utens, dtr_stage):
 def test_vadv(target):
     if target == "none":
         verifier = SDFGVerification(
-            verification={"MAP": 141, "SEQUENTIAL": 141, "FOR": 165, "Malloc": 69}
+            verification={"SEQUENTIAL": 141, "FOR": 164, "MAP": 141, "Malloc": 69}
         )
     elif target == "sequential":
         verifier = SDFGVerification(
             verification={
                 "HIGHWAY": 35,
-                "MAP": 141,
                 "SEQUENTIAL": 106,
-                "FOR": 165,
+                "FOR": 164,
+                "MAP": 141,
                 "Malloc": 69,
             }
         )
@@ -147,9 +147,10 @@ def test_vadv(target):
         verifier = SDFGVerification(
             verification={
                 "HIGHWAY": 35,
+                "CPU_PARALLEL": 15,
+                "SEQUENTIAL": 91,
+                "FOR": 164,
                 "MAP": 141,
-                "SEQUENTIAL": 106,
-                "FOR": 165,
                 "Malloc": 69,
             }
         )
