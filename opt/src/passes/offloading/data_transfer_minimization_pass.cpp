189e7181ab0e2272ed3b4eb675e4d3e61fb978da
Ramon Wirsch <ramon.wirsch@daisytuner.com>
2026-04-10 17:54:17 +0200

    Updated dev-ptr mapping when removing h2d nodes to match legacy transfer elimination changes

--- opt/src/passes/offloading/data_transfer_minimization_pass.cpp
+++ opt/src/passes/offloading/data_transfer_minimization_pass.cpp
@@ -57,8 +57,8 @@ bool DataTransferMinimizationPass::eliminate_transfer(
 
     // Maps the device pointers if necessary
     if (copy_out_device_container != copy_in_device_container) {
-        types::Pointer void_pointer;
-        types::Pointer void_pointer_pointer(*void_pointer.clone());
+        auto& container_type = builder.subject().type(copy_out_device_container);
+        auto ref_type = container_type.clone();
         auto& in_access = builder.add_access(*copy_in_block, copy_out_device_container, copy_out_src_debinfo);
         auto& out_access = builder.add_access(*copy_in_block, copy_in_device_container, copy_in_dst_debinfo);
         builder.add_reference_memlet(
@@ -66,7 +66,7 @@ bool DataTransferMinimizationPass::eliminate_transfer(
             in_access,
             out_access,
             {symbolic::zero()},
-            void_pointer_pointer,
+            *ref_type,
             DebugInfo::merge(copy_out.node->debug_info(), copy_in.node->debug_info())
         );
     }
