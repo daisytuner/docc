8f2fd9143e34ace01d8e88e86a7a75f6c0793403
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2026-07-22 23:35:13 +0200

    Added mapping for clone operation in PyTorch frontend

--- pytorch/docc/pytorch/graph_parser/tensor.py
+++ pytorch/docc/pytorch/graph_parser/tensor.py
@@ -3,8 +3,9 @@ GraphParser module for parsing operations performed directly on a tensor object.
 """
 
 import torch.fx
+from torch.fx.node import Argument
 
-from docc.sdfg import StructuredSDFGBuilder
+from docc.sdfg import StructuredSDFGBuilder, Tensor, DebugInfo
 
 from docc.pytorch.graph_parser.utils import (
     ContainerInfoBase,
@@ -15,6 +16,49 @@ from docc.pytorch.graph_parser.utils import (
     register_pre_module,
     register_module,
 )
+
+
+class CloneParser(GraphParserModule):
+    def parse(
+        self,
+        node: torch.fx.Node,
+        builder: StructuredSDFGBuilder,
+        container_info: ContainerInfos,
+    ) -> None:
+        if len(node.args) != 1:
+            raise GraphParserError(
+                self, node, "Expected 2 arguments but got: " + str(len(node.args))
+            )
+        if "memory_format" in node.kwargs:
+            memory_format: Argument = node.kwargs["memory_format"]
+            if not isinstance(memory_format, torch.memory_format):
+                raise GraphParserError(
+                    self,
+                    node,
+                    "Expected memory_format kwarg to be torch.memory_format type but got: "
+                    + str(type(memory_format)),
+                )
+            if memory_format not in [torch.contiguous_format, torch.preserve_format]:
+                raise GraphParserError(
+                    self, node, "Unsupported memory_format: " + str(memory_format)
+                )
+        elif len(node.kwargs) != 0:
+            raise GraphParserError(
+                self, node, "Unsupported kwargs: " + str(node.kwargs)
+            )
+        self_container: str = self.get_arg_container(node, container_info, 0)
+        self_tensor: Tensor = self.get_tensor_type(node, container_info, self_container)
+        result_container: str = self.get_result_container(node, builder, container_info)
+        result_tensor: Tensor = self.get_tensor_type(
+            node, container_info, result_container
+        )
+        debug_info: DebugInfo = self.get_debug_info(node)
+        builder.add_copy_op(
+            self_container, self_tensor, result_container, result_tensor, debug_info
+        )
+
+
+register_module("aten.clone.default", CloneParser())
 
 
 class ViewParser(GraphParserModule):
