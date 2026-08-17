4e5a0ea88dc2b3b209d032d4d0177681bd22944e
Washim Neupane <washimneupane@outlook.com>
2026-08-17 13:24:23 +0200

    Revert "fixed issue where the return tuple had positions swapped"
    This reverts commit 53df3f09f8e6145c72a73d5cfedc3ac6638c3865.

--- python/docc/compiler/compiled_sdfg.py
+++ python/docc/compiler/compiled_sdfg.py
@@ -301,7 +301,6 @@ class CompiledSDFG:
 
         # For output ordering (avoid sorting at runtime)
         output_order = []
-        output_name_to_info_idx = {}
 
         for i, arg_name in enumerate(self.arg_names):
             if arg_name in output_args_set:
@@ -358,7 +357,7 @@ class CompiledSDFG:
                     if self._sort_output_args:
                         output_order.append((int(arg_name.split("_")[-1]), info_idx))
                     else:
-                        output_name_to_info_idx[arg_name] = info_idx
+                        output_order.append(info_idx)
                 else:
                     # Scalar return
                     info_idx = len(self._arg_info)
@@ -368,7 +367,7 @@ class CompiledSDFG:
                     if self._sort_output_args:
                         output_order.append((int(arg_name.split("_")[-1]), info_idx))
                     else:
-                        output_name_to_info_idx[arg_name] = info_idx
+                        output_order.append(info_idx)
 
             elif arg_name.startswith("_s") and arg_name[2:].isdigit():
                 # Shape symbol argument - tuple: (arg_type, s_idx, key_str)
@@ -421,11 +420,7 @@ class CompiledSDFG:
             output_order.sort(key=lambda x: x[0])
             self._output_order = tuple(idx for _, idx in output_order)
         else:
-            self._output_order = tuple(
-                output_name_to_info_idx[name]
-                for name in self.output_args
-                if name in output_name_to_info_idx
-            )
+            self._output_order = output_order
 
         # Map from _arg_info index to result position (for O(1) lookup)
         self._output_pos_map = {idx: pos for pos, idx in enumerate(self._output_order)}
