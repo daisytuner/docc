5a0936eaad49d6c7a41dd78b2fefad95444f5b03
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2026-09-11 22:46:42 +0200

    Improved ArangeNode
    - Added flop values
    - Adapted the expansion to prevent multiplication of step if step is one and addition of start if start is zero
    - Bumped test coverage

--- sdfg/include/sdfg/data_flow/library_nodes/math/tensor/arange_node.h
+++ sdfg/include/sdfg/data_flow/library_nodes/math/tensor/arange_node.h
@@ -58,6 +58,12 @@ public:
     std::unique_ptr<data_flow::DataFlowNode>
     clone(size_t element_id, const graph::Vertex vertex, data_flow::DataFlowGraph& parent) const override;
 
+    /**
+     * @brief Calculate floating point operations for this node
+     * @return Symbolic expression for FLOP count
+     */
+    virtual symbolic::Expression flop() const override;
+
     data_flow::PointerAccessType pointer_access_type(int input_idx) const override;
 };
 
